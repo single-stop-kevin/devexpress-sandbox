e6d9ed600ab09cb5af0dc2f9b94b36c52b3bdb16
Kevin Y. Kim <kkim@singlestop.org>
2020-02-11 11:30:58 -0500

    got first devextreme button going

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import "devextreme/dist/css/dx.common.css";
+import "devextreme/dist/css/dx.light.css";
 import React from "react";
 
-function App() {
+import Button from "devextreme-react/button";
+
+const App = () => {
+  const sayHelloWorld = () => {
+    alert("Hello World!");
+  };
+
   return (
     <>
       <div>hi</div>
       <div>hello</div>
+      <Button text="Click me" onClick={sayHelloWorld} />
     </>
   );
-}
+};
 
 export default App;
